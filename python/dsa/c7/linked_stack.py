23acb3f24cc3004785e668a12e196bf3e5bcf74d
vtenpo <phongzed2001@gmail.com>
2023-01-31 13:35:30 +0700

    linked stack

---
+++ python/dsa/c7/linked_stack.py
@@ -0,0 +1,6 @@
+class LinkedStack:
+    """LIFO Stack implementation using a singly linked list for storage."""
+    
+    class Node:
+        """Lightweight, nonpublic class for storing a singly linked node."""
+        
